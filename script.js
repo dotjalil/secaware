20d4e3384cee176997430feb93ec0d48c6fd4133
dotjalil <mgkmohammad@gmail.com>
2023-12-22 20:34:27 +0200

    adding cross origin support

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const appEl = document.querySelector("#app");
 appEl.innerText = "App will run here";
+
+// Send a message to the parent window
+window.parent.postMessage("contentChanged", "*"); // Allow communication with any origin
