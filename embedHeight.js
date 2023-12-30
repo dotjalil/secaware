316183ba9ad79cabf242b22b67a6a3750dc56959
dotjalil <mgkmohammad@gmail.com>
2023-12-30 05:01:51 +0200

    observing body height for better embedding

--- embedHeight.js
+++ embedHeight.js
@@ -1,14 +1,26 @@
+const body = document.querySelector("body");
+
 // Calculate the page scroll height
 function calculatePageScrollHeight() {
   return document.documentElement.scrollHeight;
 }
 
-// Send a message to the parent window
-// when the page is embedded, it communicates the page height
-window.parent.postMessage(
-  {
-    contentChanged: true,
-    contentHeight: calculatePageScrollHeight(),
-  },
-  "*"
-); // Allow communication with any origin
+function sendMessage() {
+  // Send a message to the parent window
+  // when the page is embedded, it communicates the page height
+  window.parent.postMessage(
+    {
+      contentChanged: true,
+      contentHeight: calculatePageScrollHeight(),
+    },
+    "*"
+  ); // Allow communication with any origin
+}
+
+const observer = new ResizeObserver(function () {
+  console.log("New scrollHeight", body.scrollHeight);
+  sendMessage();
+});
+
+// This is the critical part: We observe the size of all children!
+observer.observe(body);
